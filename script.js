e208598f892491cb908bc94e9bfea0d045124c3a
Drew Lovato <gaethlovato@gmail.com>
2022-07-11 09:20:26 -0600

    css styles look finished

---
+++ script.js
@@ -0,0 +1,18 @@
+//create var for each question/answer asked
+const start_btn = document.querySelector(".start_btn button");
+const rules_box = document.querySelector(".rules_box");
+const exit_btn = document.querySelector(".buttons .stop");
+const begin_btn = document.querySelector(".buttons .begin");
+
+//FUNCTIONS
+//IF START BUTTON CLICKED =>
+start_btn.onclick = () => {
+  rules_box.classList.add("activeInfo");
+};
+//logic/call to function
+
+//logic for timer
+
+//log high scores with initial for player
+
+//reset and play again
